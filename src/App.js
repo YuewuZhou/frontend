ccd4a50f9f294d2d2eb2855c9fba7bff309b75ad
Yuewu Zhou <yzhou50@gmail.com>
2023-07-25 12:23:43 -0500

    init frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
+import React, {Component, useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import styles from './hostit-html/css/style.css'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App () {
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        fetch('/api/hello')
+            .then(response => response.text())
+            .then(message => {
+                setMessage(message);
+            });
+    },[])
+
+    const [message1, setMessage1] = useState("");
+    
+    useEffect(() => {
+      fetch('/api/goodbye')
+          .then(response => response.text())
+          .then(message1 => {
+              setMessage1(message1);
+          });
+    },[])
+
+    
+
+    return (
+        <div className="App">
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className="App-title">{message}</h1>
+        </header>
+        <Navbar/>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route path="/about"  element={<About/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route element={<NotFound/>} />
+          </Routes>
+        </Router>
+        
+        <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React, {Component, useState, useEffect} from 'react';
+
+
+const About = () => {
+  return(
+    <h1>About Us</h1>
+  )
+
+};
+
+export default About;
